b5e5d5f384c5a7bf7a301e11d805452b0b07ed05
AlessandroBianco00 <alessandrobianco2000@gmail.com>
2024-06-07 16:47:32 +0200

    aggiunto bottone in home + modifiche estetiche

--- esercitazione_11settimana/film_ng/src/app/pages/home/home.component.ts
+++ esercitazione_11settimana/film_ng/src/app/pages/home/home.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './../../services/auth.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+
+  constructor(private AuthSvc:AuthService) {
+
+  }
+
+  syncIsLoggedIn:boolean = this.AuthSvc.syncIsLoggedIn
 
 }
