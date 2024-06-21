4aa9a8c925da2bdd2d69c76657292c68c1caf32a
AlessandroBianco00 <alessandrobianco2000@gmail.com>
2024-06-21 15:05:54 +0200

    Risoluzione esercitazione w13

---
+++ esercitazione_13settimana/CalcolatoreImposta/Contribuente.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcolatoreImposta
+{
+    enum Sesso
+    {
+        M,
+        F
+    }
+    internal class Contribuente
+    {
+        public string Name {  get; set; }
+        public string Surname { get; set; }
+        public DateOnly BirthDate { get; set; }
+        public Sesso Gender { get; set; }
+        public string ComuneResidenza { get; set; }
+        public string CodiceFiscale { get; set; }
+        public int AnnualSalary { get; set; }
+
+        public Contribuente(string name, string surname, DateOnly birthDate, Sesso gender, string comuneResidenza, string codiceFiscale, int annualSalary)
+        {
+            Name = name;
+            Surname = surname;
+            BirthDate = birthDate;
+            Gender = gender;    
+            ComuneResidenza = comuneResidenza;
+            CodiceFiscale = codiceFiscale;
+            AnnualSalary = annualSalary;
+        }
+
+        public int PayTaxes()
+        {
+            int mysalary = AnnualSalary;
+            int taxes = 0;
+            if (mysalary <= 15000) { taxes = mysalary * 23 / 100; }
+            else if (mysalary <= 28000) { taxes = 3450 + ((mysalary - 15000) * 27 / 100); }
+            else if (mysalary <= 55000) { taxes = 6960 + ((mysalary - 28000) * 38 / 100); }
+            else if (mysalary <= 75000) { taxes = 17220 + ((mysalary - 55000) * 41 / 100); }
+            else { taxes = 25420 + ((mysalary - 75000) * 43 / 100); }
+            return taxes;
+        }   
+    }
+}
---
+++ esercitazione_13settimana/CalcolatoreImposta/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace CalcolatoreImposta
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("===============CALCOLO IRPEF==============");
+                Console.WriteLine("Inserisci i tuoi dati per sapere l'ammontare di tasse da pagare");
+
+                Console.WriteLine("Inserisci il tuo nome");
+                string myName = Console.ReadLine();
+
+                Console.WriteLine("Inserisci il tuo cognome");
+                string mySurname = Console.ReadLine();
+
+                DateOnly date = default;
+                try
+                {
+                    Console.WriteLine("Anno di nascita");
+                    int birtYear = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Mese di nascita");
+                    int birthMonth = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Giorno di nascita");
+                    int birthDay = int.Parse(Console.ReadLine());
+                    date = new DateOnly(birtYear, birthMonth, birthDay);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Errore: {ex.Message}");
+                }
+                Sesso myGender = default;
+                try
+                {
+                    Console.Write("Gender (M/F): ");
+                    myGender = (Sesso)Enum.Parse(typeof(Sesso), Console.ReadLine(), true);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Errore: {ex.Message}");
+                }
+                Console.WriteLine("Luogo di residenza");
+                string myComune = Console.ReadLine();
+                Console.WriteLine("Inserisci Codice Fiscale");
+                string myCodiceFisc = Console.ReadLine();
+                Console.WriteLine("Reddito Dichiarato");
+                int mySalary = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("========================================");
+
+                Contribuente contribuente1 = new Contribuente(myName, mySurname, date, myGender, myComune, myCodiceFisc, mySalary);
+                Console.WriteLine("Scheda dati");
+                Console.WriteLine($"Contribuente: {contribuente1.Name} {contribuente1.Surname},");
+                Console.WriteLine($"Nato il: {contribuente1.BirthDate} ({contribuente1.Gender}),");
+                Console.WriteLine($"Residente a: {contribuente1.ComuneResidenza},");
+                Console.WriteLine($"Codice Fiscale: {contribuente1.CodiceFiscale}");
+                Console.WriteLine($"Declared income: {contribuente1.AnnualSalary}");
+                Console.WriteLine($"La tua imposta di reddito per quest'anno è di {contribuente1.PayTaxes()}");
+                Console.WriteLine("========================================");
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
